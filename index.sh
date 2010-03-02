8008072207b909f4174f9265ccec52fa7e2ddcfa
Ping Yin <ping@adsnexus.com>
2010-03-02 12:04:24 +0800

    Initial add

---
+++ index.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#sphinx_dir=/var/lib/sphinx
+#sudo mkdir $sphinx_dir
+#sudo chown -R ping:ping $sphinx_dir
+#mkdir $sphinx_dir/{log,data}
+
+/usr/local/sphinx/bin/searchd -c sphinx.conf
+/usr/local/sphinx/bin/indexer --config sphinx.conf --all --rotate
+#/usr/local/sphinx/bin/indexer --config sphinx.conf --all
