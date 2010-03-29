09b6df6302d55695753c241d33d8ac574f8a1f3f
Ping Yin <ping@adsnexus.com>
2010-03-29 10:22:16 +0800

    index.sh: don't start searchd which should be started by cron

--- index.sh
+++ index.sh
@@ -5,6 +5,6 @@
 #sudo chown -R ping:ping $sphinx_dir
 #mkdir $sphinx_dir/{log,data}
 
-/usr/local/sphinx/bin/searchd -c sphinx.conf
+#/usr/local/sphinx/bin/searchd -c sphinx.conf
 /usr/local/sphinx/bin/indexer --config sphinx.conf --all --rotate
 #/usr/local/sphinx/bin/indexer --config sphinx.conf --all
